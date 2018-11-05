58991c05b0f93050e1879ac9f0822a214bd63615
frederick111 <295124916@qq.com>
2018-11-05 23:29:06 +0800

    Initial commit

---
+++ src/main/java/com/wangzhixuan/controller/Test.java
@@ -0,0 +1,15 @@
+package com.wangzhixuan.controller;
+
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+
+public class Test {
+
+    public  static   void  main(String args[]){
+//      EhCacheManagerFactoryBean factoryBean=new EhCacheManagerFactoryBean();
+//        factoryBean.EhCacheCacheManager ehCacheCacheManager=new EhCacheCacheManager();
+//        ehCacheCacheManager.setCacheManager(factoryBean);
+
+
+    }
+}
